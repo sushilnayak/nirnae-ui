f0771cb62ea6d456950ad6f77e8a99f8228b4c7e
Sushil Nayak <sushil.nayak@gmail.com>
2019-11-01 02:42:50 +0530

    added lasso feature to canvas

--- src/index.js
+++ src/index.js
@@ -21,8 +21,11 @@ import { Resizable } from "re-resizable";
 
 class Canvas {
   constructor(element) {
+    this.lasso = null;
+    this.mouse_position = null;
+
     this.gridSize = 20;
-    this.svg = d3.select(element).append("svg")
+    let svg = d3.select(element).append("svg")
       .attr("class", "outerCanvas")
       .attr("height", "100%")
       .attr("width", "100%")
@@ -32,10 +35,101 @@ class Canvas {
         d3.event.preventDefault();
       });
 
-    this.canvasHeight = parseInt(this.svg.style("height"), 10);
-    this.canvasWidth = parseInt(this.svg.style("width"), 10);
+    this.canvasHeight = parseInt(svg.style("height"), 10);
+    this.canvasWidth = parseInt(svg.style("width"), 10);
 
-    this.vis = this.svg.append("g")
+
+    //
+    //     mouseenter: Fires when the mouse enters the canvas.
+    //     mouseleave: Fires when the mouse leaves the canvas.
+    //     mousemove: Fires on any mouse movement over the canvas.
+    //     mouseout: Fires when the mouse leaves the canvas or any of its children.
+    //     mouseover: Fires when the mouse enters the canvas or any of its children.
+    //
+    // svg.on("mouseenter", () => console.log([d3.event.pageX, d3.event.pageY]))
+    //     .on("mouseleave", () => console.log([d3.event.pageX, d3.event.pageY]))
+    //     .on("mousemove", () => console.log([d3.event.pageX, d3.event.pageY]))
+    //     .on("mouseout", () => console.log([d3.event.pageX, d3.event.pageY]))
+    //     .on("mouseover", () => console.log([d3.event.pageX, d3.event.pageY]))
+
+    svg.on("mousedown", function (d) {
+      console.log("xxxxxxxx")
+
+      // if (this.lasso) {
+      //     this.lasso.remove();
+      //     this.lasso = null;
+      // }
+
+      let point = d3.mouse(this);
+
+      this.lasso = svg.append("rect")
+        .attr("ox", point[0])
+        .attr("oy", point[1])
+        .attr("rx", 1)
+        .attr("ry", 1)
+        .attr("x", point[0])
+        .attr("y", point[1])
+        .attr("width", 0)
+        .attr("height", 0)
+        .attr("class", "nr-ui-view-lasso");
+      d3.event.preventDefault();
+    })
+
+    svg.on("mousemove", function (d) {
+
+      this.mouse_position = d3.touches(this)[0] || d3.mouse(this);
+
+      if (this.lasso) {
+        let ox = parseInt(this.lasso.attr("ox"));
+        let oy = parseInt(this.lasso.attr("oy"));
+        let x = parseInt(this.lasso.attr("x"));
+        let y = parseInt(this.lasso.attr("y"));
+        let w;
+        let h;
+        if (this.mouse_position[0] < ox) {
+          x = this.mouse_position[0];
+          w = ox - x;
+        } else {
+          w = this.mouse_position[0] - x;
+        }
+        if (this.mouse_position[1] < oy) {
+          y = this.mouse_position[1];
+          h = oy - y;
+        } else {
+          h = this.mouse_position[1] - y;
+        }
+        this.lasso
+          .attr("x", x)
+          .attr("y", y)
+          .attr("width", w)
+          .attr("height", h)
+        ;
+        return;
+      }
+    })
+
+    svg.on("mouseup", function (d) {
+      if (this.lasso) {
+        let x = parseInt(this.lasso.attr("x"));
+        let y = parseInt(this.lasso.attr("y"));
+        let x2 = x + parseInt(this.lasso.attr("width"));
+        let y2 = y + parseInt(this.lasso.attr("height"));
+
+        console.log(x , y , x2, y2)
+        // TODO : Add logic to get all the nodes to that are there in the range
+        this.lasso.remove();
+        this.lasso = null;
+      }
+    })
+
+    svg.on("mouseleave",function (d) {
+      if (this.lasso) {
+        this.lasso.remove();
+        this.lasso = null;
+      }
+    })
+
+    this.vis = svg.append("g")
       .on("dblclick.zoom", null)
       .append("g")
       .attr("class", "innerCanvas");
@@ -161,15 +255,16 @@ function EditorLeftSidebar() {
 
   const leftSideDrawersSetters = [setComponentSidebar, setProjectSidebar];
 
+
   return <Fragment>
     <div className={"sidebar-left noselect"}>
       <div className={"rotate-left"}>
-        <div onClick={e => {
+        <div  style={{backgroundColor: projectSidebar ? "#BDBDBD" : ""}} onClick={e => {
           closeAllSideDrawer(leftSideDrawersSetters);
           setProjectSidebar(!projectSidebar);
         }}><span><FontAwesomeIcon size="sm" icon={faAlignJustify}/></span>Project
         </div>
-        <div onClick={e => {
+        <div style={{backgroundColor: componentSidebar ? "#BDBDBD" : ""}} onClick={e => {
           closeAllSideDrawer(leftSideDrawersSetters);
           setComponentSidebar(!componentSidebar);
         }}><span><FontAwesomeIcon size="sm" icon={faFolder}/></span>Components
@@ -216,22 +311,22 @@ function EditorFooter() {
 
   return <Fragment>
     <div className={"footer noselect"}>
-      <div onClick={e => {
+      <div  style={{backgroundColor: statusFooter ? "#BDBDBD" : ""}}  onClick={e => {
         closeAllSideDrawer(footerDrawersSetters);
         setStatusFooter(!statusFooter);
       }}><span><FontAwesomeIcon size="sm" icon={faCheck}/></span>Status
       </div>
-      <div onClick={e => {
+      <div  style={{backgroundColor: warningErrorFooter ? "#BDBDBD" : ""}} onClick={e => {
         closeAllSideDrawer(footerDrawersSetters);
         setWarningErrorFooter(!warningErrorFooter);
       }}><span><FontAwesomeIcon size="sm" icon={faExclamationTriangle}/></span>Warning &amp; Errors
       </div>
-      <div onClick={e => {
+      <div  style={{backgroundColor: statisticsFooter ? "#BDBDBD" : ""}} onClick={e => {
         closeAllSideDrawer(footerDrawersSetters);
         setStatisticsFooter(!statisticsFooter);
       }}><span><FontAwesomeIcon size="sm" icon={faChartBar}/></span>Statistics
       </div>
-      <div onClick={e => {
+      <div  style={{backgroundColor: controlFlowFooter ? "#BDBDBD" : ""}} onClick={e => {
         closeAllSideDrawer(footerDrawersSetters);
         setControlFlowFooter(!controlFlowFooter);
       }}><span><FontAwesomeIcon size="sm" icon={faWind}/></span>Control Flow
